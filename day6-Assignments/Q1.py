42594fab3b8883739cbf36413e60183484127265
djsagar181 <djsagar181@gmail.com>
2020-09-12 16:45:57 +0530

    Day 6 Assignments

---
+++ day6-Assignments/Q1.py
@@ -0,0 +1,43 @@
+# class for Banck_Account 
+
+class BankAccount: 
+
+    def __init__(self): 
+
+        self.ownerName="Sagar Kote"
+        
+        self.Balance=0
+        
+    def deposit(self): 
+        
+        Amount=float(input("\nEnter amount to be Deposited : ")) 
+        
+        self.Balance += Amount 
+        
+        print("\nAmount Deposited is :",Amount) 
+  
+    def withdraw(self): 
+        
+        Amount = float(input("\nEnter amount to be Withdrawn : ")) 
+        
+        if self.Balance >= Amount: 
+        
+            self.Balance -= Amount 
+            
+            print("\nYou have Withdrew :", Amount) 
+        
+        else: 
+             print("\nInsufficient balance in the account...") 
+             print("\n---WELCOME TO BACK ACCOUNT PROGRAM---")
+    
+BA = BankAccount()
+
+print("\nAccount Holder Name is :",BA.ownerName)
+    
+print("\nInitial Account Balance is :",BA.Balance)
+   
+BA.deposit();
+    
+BA.withdraw();
+    
+print("\nNet Avaliable balance is :",BA.Balance)
