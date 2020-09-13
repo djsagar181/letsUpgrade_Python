b9a1b32c74c2b4f0a94395a8b32d3348356b7c11
djsagar181 <djsagar181@gmail.com>
2020-09-13 15:17:50 +0530

    Day 8 assignments

---
+++ day8-Assignments/Q1_decorator.py
@@ -0,0 +1,29 @@
+# Decorator
+
+def getInput(calculate_arg_fun):
+    
+    def wrap_function():
+    
+        a = int(input("\nEnter First Number - "))
+        
+        b = int(input("\nEnter Second Number - "))
+        
+        calculate_arg_fun(a,b)   
+    
+    return wrap_function
+
+# calling decorator for input
+
+@getInput
+
+def EvenFinder(start, last):
+    
+    print("\nThe Even numbers in the range",start,"to",last,"is :\n")
+
+    for n in range(start, last + 1): 
+      
+        if n % 2 == 0: 
+            
+            print(n, end = " ")
+
+print(EvenFinder())
